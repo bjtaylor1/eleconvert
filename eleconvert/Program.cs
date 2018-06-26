ba7e552cb5249f33b9cbe8e412eaac6272601895
Ben Taylor <bentaylortheonly@gmail.com>
2018-06-26 20:06:17 +0100

    gets some data

--- eleconvert/Program.cs
+++ eleconvert/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace eleconvert
 {
@@ -10,8 +17,109 @@ namespace eleconvert
     {
         static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Running");
+            using (var dbconnection = new SqlConnection("Data Source=localhost;Integrated Security=SSPI;Initial Catalog=elevation;MultipleActiveResultSets=True"))
+            {
+                await dbconnection.OpenAsync().ConfigureAwait(false);
+                var batches = new List<int>();
+                using (var batchQuery = new SqlCommand("select distinct batch from roads", dbconnection))
+                {
+                    using (var batchReader = await batchQuery.ExecuteReaderAsync().ConfigureAwait(false))
+                    {
+                        while (await batchReader.ReadAsync().ConfigureAwait(false))
+                        {
+                            batches.Add(await batchReader.GetFieldValueAsync<int>(batchReader.GetOrdinal("batch")));
+                        }
+                    }
+                }
+
+                const string failedBatchesFile = "failedbatches.txt";
+                if(File.Exists(failedBatchesFile))
+                {
+                    File.Delete(failedBatchesFile);
+                }
+                batches = batches.Take(1).ToList();
+                using (var httpClient = new HttpClient { BaseAddress = new Uri("https://api.open-elevation.com/") })
+                {
+                    var tasks = batches.Select(async batch =>
+                    {
+                        try
+                        {
+                            var locations = new ConcurrentBag<Location>();
+                            using (var latLongsQuery = new SqlCommand("select lat,lon from roads where batch = @batch", dbconnection))
+                            {
+                                latLongsQuery.Parameters.AddWithValue("@batch", batch);
+                                using (var latLongsReader = await latLongsQuery.ExecuteReaderAsync().ConfigureAwait(false))
+                                {
+                                    while (await latLongsReader.ReadAsync().ConfigureAwait(false))
+                                    {
+                                        var lat = await latLongsReader.GetFieldValueAsync<string>(latLongsReader.GetOrdinal("lat")).ConfigureAwait(false);
+                                        var lon = await latLongsReader.GetFieldValueAsync<string>(latLongsReader.GetOrdinal("lon")).ConfigureAwait(false);
+                                        locations.Add(new Location { Latitude = lat, Longitude = lon });
+                                    }
+                                }
+                                var request = new Request { Locations = locations.ToArray() };
+                                var jsonRequestString = JsonConvert.SerializeObject(request, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() })
+                                    .Replace("\"", "")
+                                    .Replace("longitude", "\"longitude\"")
+                                    .Replace("latitude", "\"latitude\"")
+                                    .Replace("locations", "\"locations\"");
+                                var httpResponse = await httpClient.PostAsync("api/v1/lookup", new StringContent(jsonRequestString, Encoding.UTF8, "application/json"));
+
+                                httpResponse.EnsureSuccessStatusCode();
+                                var responseString = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+                                var responseData = JsonConvert.DeserializeObject<Response>(responseString);
+
+                                using (var updateCommand = new SqlCommand("update roads set Elevation=@ele where lat = @lat and lon = @lon", dbconnection))
+                                {
+                                    var latParam = updateCommand.Parameters.Add("@lat", SqlDbType.VarChar, 255);
+                                    var lonParam = updateCommand.Parameters.Add("@lon", SqlDbType.VarChar, 255);
+                                    var eleParam = updateCommand.Parameters.Add("@ele", SqlDbType.Int);
+                                    foreach (var result in responseData.Results)
+                                    {
+                                        latParam.Value = result.Latitude;
+                                        lonParam.Value = result.Longitude;
+                                        eleParam.Value = result.Elevation;
+                                        await updateCommand.ExecuteNonQueryAsync().ConfigureAwait(false);
+                                        Console.Write(".");
+                                    }
+                                    Console.WriteLine("!");
+                                }
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            File.AppendAllLines(failedBatchesFile, new[] { $"{batch}: {e.ToString()}", string.Empty });
+                        }
+                    }).ToArray();
+                    Task.WaitAll(tasks);
+                }
+
+                Console.WriteLine();
+            }
             return 0;
         }
+    }
+
+    public class Request
+    {
+        public Location[] Locations { get; set; }
+    }
+
+    public class Location
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+
+    public class Response
+    {
+        public Result[] Results { get; set; }
+    }
+
+    public class Result
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public int Elevation { get; set; }
     }
 }
---
+++ eleconvert/Queries.sql
@@ -0,0 +1,15 @@
+﻿
+alter table roads add id int identity(1,1) primary key clustered
+
+alter table roads add elevation bigint
+
+alter table roads add batch int
+update roads set batch = id/1000 
+alter table roads alter column batch int not null
+create index idx_batch on roads(batch)
+
+select batch, count(*) from roads group by batch
+
+select distinct id/1000 from roads
+
+select * from roads where elevation is not null
